70bed9066567fcc19842a807b35649c9ed236b2c
valiyadaksh1379 <valiyadaksh1379@gmail.com>
2025-04-22 05:56:12 +0000

    I wanted to flex a bit.

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import random
 import sys
 import base64
-
-def encode_base64(text):
-    text_bytes = text.encode('ascii')
-    base64_bytes = base64.b64encode(text_bytes)
-    return base64_bytes.decode('ascii')
-
-def decode_base64(base64_string):
-    base64_bytes = base64_string.encode('ascii')
-    text_bytes = base64.b64decode(base64_bytes)
-    return text_bytes.decode('ascii').upper()
+from encription import encode_base64
+from encription import decode_base64
 
 def choose_word(difficulty):
     words = []
